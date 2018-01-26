7c7cb3a804cdc1ec4dabe085bc135a38002fb77a
Frankziyi <617328440@qq.com>
2018-01-26 10:57:28 +0800

    get feature and test

---
+++ scripts/eval.sh
@@ -0,0 +1,32 @@
+
+# python eval.py \
+# --label_gallery_path=/home/yuanziyi/multiscale/multiscale_master/multiscale/concat_gallery_labels.mat \
+# --feature_gallery_path=/home/yuanziyi/multiscale/multiscale_master/multiscale/concat_gallery_features.mat \
+# --label_probe_path=/home/yuanziyi/multiscale/multiscale_master/multiscale/concat_probe_labels.mat \
+# --feature_probe_path=/home/yuanziyi/multiscale/multiscale_master/multiscale/concat_probe_features.mat \
+# --cam_gallery_path=/home/yuanziyi/multiscale/multiscale_master/multiscale/59286/testCAM.mat \
+# --cam_probe_path=/home/yuanziyi/multiscale/multiscale_master/multiscale/59286/queryCAM.mat 
+
+python eval.py \
+--label_gallery_path=/home/yuanziyi/RPP/RPP/54598/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/RPP/RPP/54598/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/RPP/RPP/54598/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/RPP/RPP/54598/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/RPP/RPP/54598/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/RPP/RPP/54598/queryCAM.mat 
+
+python eval.py \
+--label_gallery_path=/home/yuanziyi/RPP/RPP/91677/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/RPP/RPP/91677/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/RPP/RPP/91677/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/RPP/RPP/91677/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/RPP/RPP/91677/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/RPP/RPP/91677/queryCAM.mat 
+
+python eval.py \
+--label_gallery_path=/home/yuanziyi/RPP/RPP/99492/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/RPP/RPP/99492/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/RPP/RPP/99492/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/RPP/RPP/99492/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/RPP/RPP/99492/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/RPP/RPP/99492/queryCAM.mat 
