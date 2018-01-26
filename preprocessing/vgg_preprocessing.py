7a49fe329a94b2c9445df28cd2959267d1521d6d
Frankziyi <617328440@qq.com>
2018-01-26 13:40:19 +0800

    train with preprocess

--- preprocessing/vgg_preprocessing.py
+++ preprocessing/vgg_preprocessing.py
@@ -275,7 +275,7 @@ def _aspect_preserving_resize(image, smallest_side):
   height = shape[0]
   width = shape[1]
   new_height, new_width = _smallest_size_at_least(height, width, smallest_side)
-  image = tf.expand_dims(image, 0)
+  # image = tf.expand_dims(image, 0)
   resized_image = tf.image.resize_bilinear(image, [new_height, new_width],
                                            align_corners=False)
   resized_image = tf.squeeze(resized_image)
