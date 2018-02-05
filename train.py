3f6ca777b457fc94f91df8c9cacbdfa2433f7ea1
智慧 <zhihuikaicheng@zhihuideMBP.workgroup>
2018-02-05 19:38:54 +0800

    zkc change train

--- train.py
+++ train.py
@@ -485,7 +485,7 @@ class Trainer(object):
                     tmp_d = {}
                     for var in tmp_vars:
                         name = var.name.replace(':0', '')
-                        if name.endswith('Adam') or name.endswith('Adam_1'):
+                        if name.endswith('Adam') or name.endswith('Adam_1') or name.endswith('refined_part/biases'):
                             continue
                         tmp_d[name] = var
                     tmp_saver = tf.train.Saver(tmp_d)
