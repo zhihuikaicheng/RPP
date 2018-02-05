325a0d996cfb92c0c14533c6025c1d499d441f7b
智慧 <zhihuikaicheng@zhihuideMBP.workgroup>
2018-02-05 19:14:06 +0800

    zkc change train

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RPP
+
+### only_PCB
+0~100000
+
+### only_part_classifier
+100000~120000
+
+### all
+120000~180000
+
--- scripts/train_ResNet_PCBandRPP.sh
+++ scripts/train_ResNet_PCBandRPP.sh
@@ -39,7 +39,7 @@ python train.py \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
 --batch_size=8 \
---max_number_of_steps=180000 \
+--max_number_of_steps=160000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
 --log_dir=${LOG_DIR} \
@@ -62,7 +62,7 @@ python train.py \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
 --batch_size=8 \
---max_number_of_steps=220000 \
+--max_number_of_steps=180000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
 --log_dir=${LOG_DIR} \
