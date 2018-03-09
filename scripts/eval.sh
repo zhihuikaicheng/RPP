2506a52c2116ec07354c93e3f1d64375afd0b9f0
Frankziyi <617328440@qq.com>
2018-03-09 16:04:44 +0800

    test only rpp with softmax

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -15,26 +15,26 @@
 # --cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/143185/testCAM.mat \
 # --cam_probe_path=/home/yuanziyi/RPP_kc/RPP/143185/queryCAM.mat 
 
-# python eval.py \
-# --label_gallery_path=/home/yuanziyi/RPP_kc/RPP/151229/test_gallery_labels.mat \
-# --feature_gallery_path=/home/yuanziyi/RPP_kc/RPP/151229/test_gallery_features.mat \
-# --label_probe_path=/home/yuanziyi/RPP_kc/RPP/151229/test_probe_labels.mat \
-# --feature_probe_path=/home/yuanziyi/RPP_kc/RPP/151229/test_probe_features.mat \
-# --cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/151229/testCAM.mat \
-# --cam_probe_path=/home/yuanziyi/RPP_kc/RPP/151229/queryCAM.mat 
+python eval.py \
+--label_gallery_path=/home/yuanziyi/RPP_kc/RPP/105144/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/RPP_kc/RPP/105144/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/RPP_kc/RPP/105144/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/RPP_kc/RPP/105144/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/105144/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/RPP_kc/RPP/105144/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/home/yuanziyi/RPP_kc/RPP/137587/test_gallery_labels.mat \
---feature_gallery_path=/home/yuanziyi/RPP_kc/RPP/137587/test_gallery_features.mat \
---label_probe_path=/home/yuanziyi/RPP_kc/RPP/137587/test_probe_labels.mat \
---feature_probe_path=/home/yuanziyi/RPP_kc/RPP/137587/test_probe_features.mat \
---cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/137587/testCAM.mat \
---cam_probe_path=/home/yuanziyi/RPP_kc/RPP/137587/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/RPP_kc/RPP/118145/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/RPP_kc/RPP/118145/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/RPP_kc/RPP/118145/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/RPP_kc/RPP/118145/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/118145/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/RPP_kc/RPP/118145/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/home/yuanziyi/RPP_kc/RPP/199230/test_gallery_labels.mat \
---feature_gallery_path=/home/yuanziyi/RPP_kc/RPP/199230/test_gallery_features.mat \
---label_probe_path=/home/yuanziyi/RPP_kc/RPP/199230/test_probe_labels.mat \
---feature_probe_path=/home/yuanziyi/RPP_kc/RPP/199230/test_probe_features.mat \
---cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/199230/testCAM.mat \
---cam_probe_path=/home/yuanziyi/RPP_kc/RPP/199230/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/RPP_kc/RPP/137678/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/RPP_kc/RPP/137678/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/RPP_kc/RPP/137678/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/RPP_kc/RPP/137678/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/RPP_kc/RPP/137678/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/RPP_kc/RPP/137678/queryCAM.mat 
--- scripts/get_features_rpp.sh
+++ scripts/get_features_rpp.sh
@@ -55,7 +55,7 @@ python get_features_rpp.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=137587 \
+--ckpt_num=105144 \
 --scale_height=384 \
 --scale_width=128 \
 --GPU_use=7 \
@@ -72,9 +72,26 @@ python get_features_rpp.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=199230 \
+--ckpt_num=118145 \
 --scale_height=384 \
 --scale_width=128 \
 --GPU_use=7 \
 --only_pcb=False \
---only_classifier=False
+--only_classifier=True
+
+python get_features_rpp.py \
+--dataset_name=Market_1501 \
+--probe_dataset_dir=${PROBE_OUTPUT_DIR} \
+--gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
+--batch_size=8 \
+--max_number_of_steps=10001 \
+--checkpoint_dir=${TRAIN_DIR} \
+--pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
+--log_dir=${LOG_DIR} \
+--weight_decay=0.00004 \
+--ckpt_num=137678 \
+--scale_height=384 \
+--scale_width=128 \
+--GPU_use=7 \
+--only_pcb=False \
+--only_classifier=True
