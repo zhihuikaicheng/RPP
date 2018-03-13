a2ecb2ff41d88cea658d31bd4e771c6877cf4f4a
Frankziyi <617328440@qq.com>
2018-03-13 10:23:14 +0800

    look the refined part

--- scripts/get_features_rpp.sh
+++ scripts/get_features_rpp.sh
@@ -55,43 +55,43 @@ python get_features_rpp.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=94215 \
+--ckpt_num=198222 \
 --scale_height=384 \
 --scale_width=128 \
 --GPU_use=7 \
---only_pcb=True \
+--only_pcb=False \
 --only_classifier=False
 
-python get_features_rpp.py \
---dataset_name=Market_1501 \
---probe_dataset_dir=${PROBE_OUTPUT_DIR} \
---gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
---batch_size=8 \
---max_number_of_steps=10001 \
---checkpoint_dir=${TRAIN_DIR} \
---pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
---log_dir=${LOG_DIR} \
---weight_decay=0.00004 \
---ckpt_num=95683 \
---scale_height=384 \
---scale_width=128 \
---GPU_use=7 \
---only_pcb=True \
---only_classifier=False
+# python get_features_rpp.py \
+# --dataset_name=Market_1501 \
+# --probe_dataset_dir=${PROBE_OUTPUT_DIR} \
+# --gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
+# --batch_size=8 \
+# --max_number_of_steps=10001 \
+# --checkpoint_dir=${TRAIN_DIR} \
+# --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
+# --log_dir=${LOG_DIR} \
+# --weight_decay=0.00004 \
+# --ckpt_num=95683 \
+# --scale_height=384 \
+# --scale_width=128 \
+# --GPU_use=7 \
+# --only_pcb=True \
+# --only_classifier=False
 
-python get_features_rpp.py \
---dataset_name=Market_1501 \
---probe_dataset_dir=${PROBE_OUTPUT_DIR} \
---gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
---batch_size=8 \
---max_number_of_steps=10001 \
---checkpoint_dir=${TRAIN_DIR} \
---pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
---log_dir=${LOG_DIR} \
---weight_decay=0.00004 \
---ckpt_num=98624 \
---scale_height=384 \
---scale_width=128 \
---GPU_use=7 \
---only_pcb=True \
---only_classifier=False
+# python get_features_rpp.py \
+# --dataset_name=Market_1501 \
+# --probe_dataset_dir=${PROBE_OUTPUT_DIR} \
+# --gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
+# --batch_size=8 \
+# --max_number_of_steps=10001 \
+# --checkpoint_dir=${TRAIN_DIR} \
+# --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
+# --log_dir=${LOG_DIR} \
+# --weight_decay=0.00004 \
+# --ckpt_num=98624 \
+# --scale_height=384 \
+# --scale_width=128 \
+# --GPU_use=7 \
+# --only_pcb=True \
+# --only_classifier=False
