8db3a47511d0c4aec555d6d243dc508e4c5f9cbd
Frankziyi <617328440@qq.com>
2018-04-23 10:24:55 +0800

    train resnet v1 baseline

--- train.py
+++ train.py
@@ -420,12 +420,9 @@ class Trainer(object):
                 self.network.label:batch[1]
             }
             # calc_obj
-            if FLAGS.only_classifier:
-                calc_obj = [self.train_op_only_classifier, 
-                    self.network.sub_models[0].loss, self.network.sub_models[0].acc]
-            else:
-                calc_obj = [self.train_op, 
-                	self.network.sub_models[0].loss, self.network.sub_models[0].acc]
+            
+            calc_obj = [self.train_op, 
+                self.network.sub_models[0].loss, self.network.sub_models[0].acc]
             
             if timeout:
                 calc_obj.append(self.summary_op)
