2bfb3072127197727c2b1c4c73a581261ae0f268
Frankziyi <617328440@qq.com>
2018-04-24 11:47:02 +0800

    test lr with lower lr in step 2

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -15,20 +15,20 @@
 # --cam_probe_path=/home/yuanziyi/baseline2/RPP/143185/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/home/yuanziyi/baseline2/RPP/13948/test_gallery_labels.mat \
---feature_gallery_path=/home/yuanziyi/baseline2/RPP/13948/test_gallery_features.mat \
---label_probe_path=/home/yuanziyi/baseline2/RPP/13948/test_probe_labels.mat \
---feature_probe_path=/home/yuanziyi/baseline2/RPP/13948/test_probe_features.mat \
---cam_gallery_path=/home/yuanziyi/baseline2/RPP/13948/testCAM.mat \
---cam_probe_path=/home/yuanziyi/baseline2/RPP/13948/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/baseline2/RPP/22471/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/baseline2/RPP/22471/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/baseline2/RPP/22471/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/baseline2/RPP/22471/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/baseline2/RPP/22471/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/baseline2/RPP/22471/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/home/yuanziyi/baseline2/RPP/16726/test_gallery_labels.mat \
---feature_gallery_path=/home/yuanziyi/baseline2/RPP/16726/test_gallery_features.mat \
---label_probe_path=/home/yuanziyi/baseline2/RPP/16726/test_probe_labels.mat \
---feature_probe_path=/home/yuanziyi/baseline2/RPP/16726/test_probe_features.mat \
---cam_gallery_path=/home/yuanziyi/baseline2/RPP/16726/testCAM.mat \
---cam_probe_path=/home/yuanziyi/baseline2/RPP/16726/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/baseline2/RPP/23904/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/baseline2/RPP/23904/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/baseline2/RPP/23904/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/baseline2/RPP/23904/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/baseline2/RPP/23904/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/baseline2/RPP/23904/queryCAM.mat 
 
 # python eval.py \
 # --label_gallery_path=/home/yuanziyi/baseline2/RPP/23686/test_gallery_labels.mat \
--- scripts/get_features_rpp.sh
+++ scripts/get_features_rpp.sh
@@ -20,7 +20,7 @@ python get_features_single.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=13948 \
+--ckpt_num=22471 \
 --scale_height=256 \
 --scale_width=128 \
 --GPU_use=4 \
@@ -34,7 +34,7 @@ python get_features_single.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=16726 \
+--ckpt_num=23904 \
 --scale_height=256 \
 --scale_width=128 \
 --GPU_use=4 \
