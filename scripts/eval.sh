490f638fec67903c985604ec781b28e31fe34582
Frankziyi <617328440@qq.com>
2018-04-25 12:37:58 +0800

    test momentum

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -15,25 +15,25 @@
 # --cam_probe_path=/home/yuanziyi/baseline2/RPP/143185/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/home/yuanziyi/baseline2/RPP/22471/test_gallery_labels.mat \
---feature_gallery_path=/home/yuanziyi/baseline2/RPP/22471/test_gallery_features.mat \
---label_probe_path=/home/yuanziyi/baseline2/RPP/22471/test_probe_labels.mat \
---feature_probe_path=/home/yuanziyi/baseline2/RPP/22471/test_probe_features.mat \
---cam_gallery_path=/home/yuanziyi/baseline2/RPP/22471/testCAM.mat \
---cam_probe_path=/home/yuanziyi/baseline2/RPP/22471/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/baseline2/RPP/20696/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/baseline2/RPP/20696/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/baseline2/RPP/20696/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/baseline2/RPP/20696/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/baseline2/RPP/20696/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/baseline2/RPP/20696/queryCAM.mat 
 
 python eval.py \
---label_gallery_path=/home/yuanziyi/baseline2/RPP/23904/test_gallery_labels.mat \
---feature_gallery_path=/home/yuanziyi/baseline2/RPP/23904/test_gallery_features.mat \
---label_probe_path=/home/yuanziyi/baseline2/RPP/23904/test_probe_labels.mat \
---feature_probe_path=/home/yuanziyi/baseline2/RPP/23904/test_probe_features.mat \
---cam_gallery_path=/home/yuanziyi/baseline2/RPP/23904/testCAM.mat \
---cam_probe_path=/home/yuanziyi/baseline2/RPP/23904/queryCAM.mat 
+--label_gallery_path=/home/yuanziyi/baseline2/RPP/22073/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/baseline2/RPP/22073/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/baseline2/RPP/22073/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/baseline2/RPP/22073/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/baseline2/RPP/22073/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/baseline2/RPP/22073/queryCAM.mat 
 
-# python eval.py \
-# --label_gallery_path=/home/yuanziyi/baseline2/RPP/23686/test_gallery_labels.mat \
-# --feature_gallery_path=/home/yuanziyi/baseline2/RPP/23686/test_gallery_features.mat \
-# --label_probe_path=/home/yuanziyi/baseline2/RPP/23686/test_probe_labels.mat \
-# --feature_probe_path=/home/yuanziyi/baseline2/RPP/23686/test_probe_features.mat \
-# --cam_gallery_path=/home/yuanziyi/baseline2/RPP/23686/testCAM.mat \
-# --cam_probe_path=/home/yuanziyi/baseline2/RPP/23686/queryCAM.mat 
+python eval.py \
+--label_gallery_path=/home/yuanziyi/baseline2/RPP/23453/test_gallery_labels.mat \
+--feature_gallery_path=/home/yuanziyi/baseline2/RPP/23453/test_gallery_features.mat \
+--label_probe_path=/home/yuanziyi/baseline2/RPP/23453/test_probe_labels.mat \
+--feature_probe_path=/home/yuanziyi/baseline2/RPP/23453/test_probe_features.mat \
+--cam_gallery_path=/home/yuanziyi/baseline2/RPP/23453/testCAM.mat \
+--cam_probe_path=/home/yuanziyi/baseline2/RPP/23453/queryCAM.mat 
--- scripts/get_features_rpp.sh
+++ scripts/get_features_rpp.sh
@@ -2,7 +2,7 @@
 # Where the pre-trained InceptionV3 checkpoint is saved to.
 PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/checkpoints
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
-TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/ResNet_v1_lr
+TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/ResNet_v1_enbedding_2
 # Where the dataset is saved to.
 DATASET_DIR=/home/yuanziyi/Market-1501
 # WHere the log is saved to
@@ -20,7 +20,7 @@ python get_features_single.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=22471 \
+--ckpt_num=20696 \
 --scale_height=256 \
 --scale_width=128 \
 --GPU_use=4 \
@@ -34,21 +34,21 @@ python get_features_single.py \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
 --log_dir=${LOG_DIR} \
 --weight_decay=0.00004 \
---ckpt_num=23904 \
+--ckpt_num=22073 \
 --scale_height=256 \
 --scale_width=128 \
 --GPU_use=4 \
 
-# python get_features_single.py \
-# --dataset_name=Market_1501 \
-# --probe_dataset_dir=${PROBE_OUTPUT_DIR} \
-# --gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
-# --batch_size=32 \
-# --checkpoint_dir=${TRAIN_DIR} \
-# --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
-# --log_dir=${LOG_DIR} \
-# --weight_decay=0.00004 \
-# --ckpt_num=23686 \
-# --scale_height=256 \
-# --scale_width=128 \
-# --GPU_use=4 \
+python get_features_single.py \
+--dataset_name=Market_1501 \
+--probe_dataset_dir=${PROBE_OUTPUT_DIR} \
+--gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
+--batch_size=32 \
+--checkpoint_dir=${TRAIN_DIR} \
+--pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
+--log_dir=${LOG_DIR} \
+--weight_decay=0.00004 \
+--ckpt_num=23453 \
+--scale_height=256 \
+--scale_width=128 \
+--GPU_use=4 \
