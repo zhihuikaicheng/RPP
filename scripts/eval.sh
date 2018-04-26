c8550642ea9348df2795bf00501fde0eb242a487
智慧 <zhihuikaicheng@zhihuideMBP.workgroup>
2018-04-26 18:40:24 +0800

    zkc change get feature

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,9 @@
-FEATURE_PATH=/home/zhangkaicheng/RPP
-ckep_num1=11865
+FEATURE_PATH=/home/zhangkaicheng/RPP/11865
 
 python eval.py \
---label_gallery_path=${FEATURE_PATH}/ckep_num1/test_gallery_labels.mat \
---feature_gallery_path=${FEATURE_PATH}/ckep_num1/test_gallery_features.mat \
---label_probe_path=${FEATURE_PATH}/ckep_num1/test_probe_labels.mat \
---feature_probe_path=${FEATURE_PATH}/ckep_num1/test_probe_features.mat \
---cam_gallery_path=${FEATURE_PATH}/ckep_num1/92740/testCAM.mat \
---cam_probe_path=${FEATURE_PATH}/ckep_num1/92740/queryCAM.mat 
+--label_gallery_path=${FEATURE_PATH}/test_gallery_labels.mat \
+--feature_gallery_path=${FEATURE_PATH}/test_gallery_features.mat \
+--label_probe_path=${FEATURE_PATH}/test_probe_labels.mat \
+--feature_probe_path=${FEATURE_PATH}/test_probe_features.mat \
+--cam_gallery_path=${FEATURE_PATH}/testCAM.mat \
+--cam_probe_path=${FEATURE_PATH}/queryCAM.mat 
