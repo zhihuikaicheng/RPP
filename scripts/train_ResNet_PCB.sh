ea6a476b593ee18f499d61f84ab21986b4238ead
智慧 <zhihuikaicheng@zhihuideMacBook-Pro.local>
2018-04-27 11:48:39 +0800

    zkc change feature dim

--- scripts/train_ResNet_PCB.sh
+++ scripts/train_ResNet_PCB.sh
@@ -2,7 +2,7 @@
 # Where the pre-trained InceptionV3 checkpoint is saved to.
 PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/checkpoints
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
-TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/ResNet_v1_PCBRPP_new
+TRAIN_DIR=/world/data-gpu-94/sysu-reid/checkpoints/ResNet_v1_PCBRPP_new2
 # Where the dataset is saved to.
 DATASET_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/Market-1501
 # WHere the log is saved to
@@ -14,22 +14,22 @@ python train.py \
 --learning_rate_decay_type=fixed \
 --dataset_name=Market_1501 \
 --dataset_dir=${OUTPUT_DIR} \
---batch_size=64 \
---max_number_of_steps=8000 \
+--batch_size=32 \
+--max_number_of_steps=16000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
 --log_dir=${LOG_DIR} \
 --save_model_summary_secs=200 \
---log_every_n_steps=50 \
+--log_every_n_steps=100 \
 --optimizer=sgd \
---weight_decay=0.00004 \
+--weight_decay=5e-4 \
 --scale_height=384 \
 --scale_width=128 \
 --GPU_use=7 \
 --only_pcb=True \
 --only_classifier=False \
---max_step_to_train_pcb=12000 \
---max_step_to_train_classifier=2000
+--max_step_to_train_pcb=24000 \
+--max_step_to_train_classifier=4000
 
 python train.py \
 --learning_rate=0.01 \
@@ -37,43 +37,20 @@ python train.py \
 --dataset_name=Market_1501 \
 --dataset_split_name=train \
 --dataset_dir=${OUTPUT_DIR} \
---batch_size=64 \
---max_number_of_steps=12000 \
+--batch_size=32 \
+--max_number_of_steps=24000 \
 --checkpoint_dir=${TRAIN_DIR} \
 --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v1_50.ckpt \
 --log_dir=${LOG_DIR} \
 --save_model_summary_secs=200 \
---log_every_n_steps=50 \
+--log_every_n_steps=100 \
 --optimizer=sgd \
---weight_decay=0.00004 \
+--weight_decay=5e-4 \
 --scale_height=384 \
 --scale_width=128 \
 --GPU_use=7 \
 --only_pcb=True \
 --only_classifier=False \
---max_step_to_train_pcb=12000 \
---max_step_to_train_classifier=2000
+--max_step_to_train_pcb=24000 \
+--max_step_to_train_classifier=4000
 
-# python train.py \
-# --learning_rate=1e-5 \
-# --learning_rate_decay_type=fixed \
-# --dataset_name=Market_1501 \
-# --dataset_dir=${OUTPUT_DIR} \
-# --batch_size=8 \
-# --max_number_of_steps=100000 \
-# --checkpoint_dir=${TRAIN_DIR} \
-# --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/resnet_v2_50.ckpt \
-# --log_dir=${LOG_DIR} \
-# --save_model_summary_secs=300 \
-# --log_every_n_steps=100 \
-# --optimizer=adam \
-# --adam_beta1=0.5 \
-# --adam_beta2=0.999 \
-# --weight_decay=0.00004 \
-# --scale_height=384 \
-# --scale_width=128 \
-# --GPU_use=3 \
-# --only_pcb=True \
-# --only_classifier=False \
-# --max_step_to_train_pcb=100000 \
-# --max_step_to_train_classifier=40000
